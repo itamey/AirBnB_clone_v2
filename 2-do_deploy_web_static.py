415fe02970c68d7b90d776f4f28981fe880c102a
eyoshalom <eyoshalom@yahoo.com>
2023-04-08 21:48:31 +0300

    changed

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,7 +9,7 @@ from datetime import datetime
 from fabric.api import env, local, put, run, runs_once
 
 
-env.hosts = ['34.138.32.248', '3.226.74.205']
+env.hosts = ['34.138.32.248', '54.82.133.141']
 
 
 def do_deploy(archive_path):
